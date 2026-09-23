109913d09f3f9c288610a44c029e7b5c430fb63a
Pupyrinth <contact@pupyrinth.dev>
2026-09-23 18:07:14 +0200

    Aliases for fastfetch

--- files/system/etc/profile.d/pupzite-fastfetch.sh
+++ files/system/etc/profile.d/pupzite-fastfetch.sh
@@ -1,5 +1,4 @@
-case $- in
-  *i*)
-    fastfetch --config pupzite
-    ;;
-esac
+alias neofetch='/usr/bin/fastfetch --config pupzite'
+alias fastfetch='/usr/bin/fastfetch --config pupzite'
+alias neowofetch='/usr/bin/fastfetch --config pupzite'
+alias hyfetch='/usr/bin/hyfetch --backend="fastfetch" --args="--config pupzite"'
