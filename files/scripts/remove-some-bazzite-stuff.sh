a8394adda27f2cc9f645cc81bc704abf39f9bc06
Pupyrinth <contact@pupyrinth.dev>
2026-09-24 14:24:00 +0200

    A little bit of cleanup

--- files/scripts/remove-some-bazzite-stuff.sh
+++ files/scripts/remove-some-bazzite-stuff.sh
@@ -5,14 +5,12 @@
 # builds actually ran successfully without any errors!
 set -oue pipefail
 
+rm -f /etc/profile.d/bazzite-neofetch.sh
+rm -f /etc/profile.d/user-motd.sh
 
-rm -rf /usr/share/applications/Waydroid
-rm -f /usr/share/applications/bazzite-documentation.desktop
-rm -f /usr/share/applications/bazzite-steam-bpm.desktop
-rm -f /usr/share/applications/bbrew.desktop
-rm -f /usr/share/applications/discourse.desktop
-rm -f /usr/share/applications/system-update.desktop
-rm -f /usr/share/applications/waydroid-container-restart.desktop
+rm -f /etc/skel/.config/autostart/bazzite-portal.desktop
+
+rm -f /etc/xdg/autostart/bazzite-announcement.desktop
 
 rm -f /usr/libexec/bazzite-announcement
 rm -f /usr/libexec/waydroid-container-restart
@@ -20,11 +18,12 @@ rm -f /usr/libexec/waydroid-container-start
 rm -f /usr/libexec/waydroid-container-stop
 rm -f /usr/libexec/waydroid-fix-controllers
 
+rm -f /usr/share/applications/bazzite-documentation.desktop
+rm -f /usr/share/applications/bazzite-steam-bpm.desktop
+rm -f /usr/share/applications/bbrew.desktop
+rm -f /usr/share/applications/discourse.desktop
+rm -f /usr/share/applications/system-update.desktop
+rm -rf /usr/share/applications/Waydroid
+rm -f /usr/share/applications/waydroid-container-restart.desktop
+
 rm -f /usr/share/containers/systemd/cockpit-container.container
-
-rm -f /etc/xdg/autostart/bazzite-announcement.desktop
-
-rm -f /etc/profile.d/bazzite-neofetch.sh
-rm -f /etc/profile.d/user-motd.sh
-
-rm -f /etc/skel/.config/autostart/bazzite-portal.desktop
